2c5b0bbc42746efecc9954ae7229e5af8cb42cd4
Dylan McGee <46721669+dtmcgee-su@users.noreply.github.com>
2021-08-02 12:48:07 -0400

    Created error handling

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const{ projects }  = require('./data.json');
+
+const app = express();
+app.use(express.json());
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+//Routes
+app.get('/', (req, res, next) => {
+    // res.locals = data.projects;
+    res.render('index');
+});
+
+app.get('/about', (req, res, next) => {
+    res.render('about');
+});
+
+app.get('/projects/:id', (req, res, next) => {
+    const id = req.params.id;
+    const project = projects[id];
+    if (project){
+        res.render('project', { project })
+    } else {
+        next();
+    }
+});
+
+//Error Handling
+app.use((req, res, next) => {
+    const err = new Error('Sorry! This page does not exist!');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (err.status === 404){
+        res.locals.err = err;
+        res.status(err.status);
+        res.render('error');
+    } else {
+        err.message = 'Error: server issue';
+        res.render('error');
+    }
+ 
+    console.log(err.status, err.message);
+});
+
+app.listen(3000, () => {
+    console.log('You are live using port 3000!');
+});
